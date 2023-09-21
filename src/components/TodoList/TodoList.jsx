ab8a772c4a64a48626f7453dcfdba24f8105fe1e
iamsixwater <soup.sixwater@gmail.com>
2023-09-21 13:43:29 +1000

    add todo update/delete function

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
+import Todo from "../Todo/Todo";
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
@@ -7,15 +8,22 @@ export default function TodoList() {
     { id: "2", text: "sleep", status: "active" },
     { id: "3", text: "study", status: "active" },
   ]);
-  const handleAdd = (todo) => {
-    setTodos([...todos, todo]);
-  };
+  const handleAdd = (todo) => setTodos([...todos, todo]);
+  const handleUpdate = (updated) =>
+    setTodos(todos.map((todo) => (updated.id === todo.id ? updated : todo)));
+  const handleDelete = (deleted) =>
+    setTodos(todos.filter((todo) => deleted.id !== todo.id));
 
   return (
     <div>
       <ul>
         {todos.map((item) => (
-          <li key={item.id}>{item.text}</li>
+          <Todo
+            key={item.id}
+            todo={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
         ))}
       </ul>
       <AddTodo onAdd={handleAdd} />
