c51f61681ef361e10ae9d9613a7125c0c9f2159b
iamsixwater <soup.sixwater@gmail.com>
2023-09-21 18:21:52 +1000

    add light/dark mode

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import styles from "./Header.module.css";
+import { useDarkMode } from "../../context/DarkModeContext";
+import { BiSun, BiMoon } from "react-icons/bi";
 
 export default function Header({ filters, filter, onFilterChange }) {
+  const { darkMode, toggleDarkMode } = useDarkMode();
   return (
     <header className={styles.header}>
+      <button className={styles.toggle} onClick={toggleDarkMode}>
+        {!darkMode ? <BiSun /> : <BiMoon />}
+      </button>
       <ul className={styles.filters}>
         {filters.map((val, idx) => (
           <li key={idx}>
